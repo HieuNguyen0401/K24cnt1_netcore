07c6ac9e3ab6d3cb3f84c7b05c718659f6688db1
HieuNguyen0401 <hieu040106amp@gmail.com>
2026-08-27 22:32:58 +0700

    Delete d/d directory

--- d/d/NnhLession02demo/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace PDK_Lession02demo.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
