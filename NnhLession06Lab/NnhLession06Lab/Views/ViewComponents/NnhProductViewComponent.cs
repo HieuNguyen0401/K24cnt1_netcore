303ea7e67f1e01a81963e772902bf8c0eef16974
Nguyen Ngoc Hieu <hieu040106amp@gmail.com>
2026-09-09 14:19:28 +0700

    NnhLession06

---
+++ NnhLession06Lab/NnhLession06Lab/Views/ViewComponents/NnhProductViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NnhLession04.Models;
+
+namespace NnhLession06Lab.Views.ViewComponents
+{
+	public class HotProductViewComponent : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			var products = new List<NnhProduct>()
+			{
+				new NnhProduct
+				{
+					Id = 1,
+					Name = "Nồi cơm điện cao tần Nagakawa NAG0102",
+					Price = 2590000,
+					Image = "/images/noicom.jpg",
+					Category = "Đồ gia dụng"
+				},
+
+				new NnhProduct
+				{
+					Id = 2,
+					Name = "Nồi cơm điện cao tần Nagakawa NAG0102",
+					Price = 2590000,
+					Image = "/images/noicom.jpg",
+					Category = "Đồ gia dụng"
+				},
+
+				new NnhProduct
+				{
+					Id = 3,
+					Name = "Nồi cơm điện cao tần Nagakawa NAG0102",
+					Price = 2590000,
+					Image = "/images/noicom.jpg",
+					Category = "Đồ gia dụng"
+				}
+			};
+
+			return View(products);
+		}
+	}
+}
