f3c5f7342e3aefe7955f69a2c38f87e2eb20c26a
Nguyen Ngoc Hieu <hieu040106amp@gmail.com>
2026-09-25 09:23:08 +0700

    NnhLession10efdbfist

---
+++ NnhLession10efdbfist/NnhLession10efdbfist/Models/NnhLession10efdbContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace NnhLession10efdbfist.Models;
+
+public partial class NnhLession10efdbContext : DbContext
+{
+    public NnhLession10efdbContext()
+    {
+    }
+
+    public NnhLession10efdbContext(DbContextOptions<NnhLession10efdbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<NnhMember> NnhMembers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=NGỌCHIẾU\\SQLEXPRESS02;Database=NnhLession10efdb;Trusted_Connection=True;MultipleActiveResultSets=True;TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<NnhMember>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__NnhMembe__3214EC0740E7A604");
+
+            entity.ToTable("NnhMember");
+
+            entity.Property(e => e.Id).ValueGeneratedNever();
+            entity.Property(e => e.NnhEmail)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NnhFullName).HasMaxLength(50);
+            entity.Property(e => e.NnhPassword)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+            entity.Property(e => e.NnhPhone)
+                .HasMaxLength(12)
+                .IsUnicode(false)
+                .IsFixedLength();
+            entity.Property(e => e.NnhUserName)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ NnhLession10efdbfist/NnhLession10efdbfist/Models/NnhMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NnhLession10efdbfist.Models;
+
+public partial class NnhMember
+{
+    public long Id { get; set; }
+
+    public string? NnhUserName { get; set; }
+
+    public string? NnhPassword { get; set; }
+
+    public string? NnhFullName { get; set; }
+
+    public string? NnhEmail { get; set; }
+
+    public string? NnhPhone { get; set; }
+
+    public bool? NnhStatus { get; set; }
+}
